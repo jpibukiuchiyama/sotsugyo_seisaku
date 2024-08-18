e606a4c60a48fa8d8411159d19955e6a64213820
jpibukiuchiyama <107329785+jpibukiuchiyama@users.noreply.github.com>
2024-08-18 09:29:55 +0900

    Update api_main.py

--- sotsugyo_seisaku/api_main.py
+++ sotsugyo_seisaku/api_main.py
@@ -51,7 +51,7 @@ def main():
         if keys[pygame.K_y]: a.setchar(24,param_MCJE.GLOWSTONE,a_x,a_y,a_z)
         if keys[pygame.K_z]: a.setchar(25,param_MCJE.GLOWSTONE,a_x,a_y,a_z)
         if keys[pygame.K_LALT]: a_y = a_y - 7
-        if keys[pygame.K_LSHIFT]: a_x = 5
+        if keys[pygame.K_LSHIFT]: a_x = 0
 
         #screen.blit(image, (x, y))  # 描画
         pygame.display.flip()   # 画面フリップ
